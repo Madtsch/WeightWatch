e19ca9a451ebd2a929b17d60cd7cecd5595a21e8
Madtsch <30468854+Madtsch@users.noreply.github.com>
2018-04-30 16:21:13 +0200

    First prototype version

---
+++ Assets/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+    public InputField weightDate;
+    public InputField weightWeight;
+    private string sysdate = System.DateTime.Now.ToString("yyyy-dd-MM");
+
+    void Start () {
+        Debug.Log("I am here!");
+        weightDate.text = sysdate;
+        weightWeight.text = "104.4";
+	}
+
+    public void SaveData()
+    {
+
+    }
+}
